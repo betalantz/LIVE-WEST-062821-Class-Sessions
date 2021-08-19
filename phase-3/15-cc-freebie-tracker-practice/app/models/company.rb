34302838daf69ecfcfd408606f76b872ee3eeb0d
betalantz <betalantz@gmail.com>
2021-08-19 15:46:46 -0700

    adds #give_freebie

--- phase-3/15-cc-freebie-tracker-practice/app/models/company.rb
+++ phase-3/15-cc-freebie-tracker-practice/app/models/company.rb
@@ -3,4 +3,9 @@ class Company < ActiveRecord::Base
     has_many :freebies
     has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        # Freebie.create(dev: dev, company: self, item_name: item_name, value: value)
+        Freebie.create(dev_id: dev.id, company_id: self.id, item_name: item_name, value: value)
+    end
+
 end
