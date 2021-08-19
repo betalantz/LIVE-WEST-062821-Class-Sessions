b4fac3084c078a08c361ce40e86a5f2e1a57924c
betalantz <betalantz@gmail.com>
2021-08-19 16:04:25 -0700

    adds #received_one?

--- phase-3/15-cc-freebie-tracker-practice/app/models/dev.rb
+++ phase-3/15-cc-freebie-tracker-practice/app/models/dev.rb
@@ -3,4 +3,10 @@ class Dev < ActiveRecord::Base
     has_many :freebies
     has_many :companies, through: :freebies
 
+    def received_one?(item_name)
+        self.freebies.any? {|freebie| freebie.item_name == item_name }
+        # self.freebies.pluck(:item_name).include?(item_name)
+        # !self.freebies.where(item_name: item_name).empty?
+    end
+
 end
