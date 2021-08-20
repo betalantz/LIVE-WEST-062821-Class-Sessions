dc4058d3a012c554c13f5e6be0f077a694184cdf
betalantz <betalantz@gmail.com>
2021-08-19 18:41:34 -0700

    adds notes to #give_away

--- phase-3/15-cc-freebie-tracker-practice/app/models/dev.rb
+++ phase-3/15-cc-freebie-tracker-practice/app/models/dev.rb
@@ -9,8 +9,12 @@ class Dev < ActiveRecord::Base
         # !self.freebies.where(item_name: item_name).empty?
     end
 
-    def give_away(dev, freebie)
-        freebie.update(dev: dev)
+    def give_away(dev, freebie) # dev and freebie are instances
+        # a freebie belongs to dev1
+        # we want the freebie to belong to the passed-in dev
+        # I need to change the current dev_id on this freebie to the passed in dev_id
+        # changing can be done by an update
+        freebie.update(dev_id: dev.id)
     end
 
 end
