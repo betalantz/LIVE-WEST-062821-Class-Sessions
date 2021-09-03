c903fff3e3087bb307226c72daa76ee61e665f37
betalantz <betalantz@gmail.com>
2021-09-03 14:34:10 -0700

    gym actions complete with error handling

--- phase-4/phase-4-rails-practice-challenge-gym/app/controllers/application_controller.rb
+++ phase-4/phase-4-rails-practice-challenge-gym/app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::API
 
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response  
+
     wrap_parameters format: []
+
+    def render_not_found_response(exception)
+        render json: { error: "#{exception.model} not found" }, status: :not_found
+    end
+
 end
--- phase-4/phase-4-rails-practice-challenge-gym/app/controllers/gyms_controller.rb
+++ phase-4/phase-4-rails-practice-challenge-gym/app/controllers/gyms_controller.rb
@@ -1,2 +1,22 @@
 class GymsController < ApplicationController
+
+    before_action :find_gym
+
+    def show
+        render json: @gym
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def find_gym
+        @gym = Gym.find(params[:id])
+    end
+
+    # def render_not_found_response
+    #     render json: { error: "Gym not found" }, status: :not_found
+    # end
 end
