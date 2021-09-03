2554d1971fd0fd14a0eee221a1427ad2f86ead60
betalantz <betalantz@gmail.com>
2021-09-03 09:35:09 -0700

    all deliverables met

--- phase-4/phase-4-mock-challenge-camping-fun/app/controllers/campers_controller.rb
+++ phase-4/phase-4-mock-challenge-camping-fun/app/controllers/campers_controller.rb
@@ -1,20 +1,28 @@
 class CampersController < ApplicationController
+    
 
     def index
         render json: Camper.all
     end
 
     def show
-
+        camper = Camper.find(params[:id])
+        render json: camper
     end
 
     def create
-
+        camper = Camper.create!(camper_params)
+        render json: camper, status: :created
     end
 
     private
 
     def camper_params
+        params.permit(:name, :age)
+    end
 
+    
+    def render_not_found_response
+        render json: { error: "Camper not found" }, status: :not_found
     end
 end
--- phase-4/phase-4-mock-challenge-camping-fun/app/models/camper.rb
+++ phase-4/phase-4-mock-challenge-camping-fun/app/models/camper.rb
@@ -3,5 +3,5 @@ class Camper < ApplicationRecord
     has_many :activities, through: :signups
 
     validates :name, presence: true
-    validates :age, inclusion: 8..18
+    validates :age, inclusion: { in: 8..18, message: "You done too old!"}
 end
