43bf315425e5a5e0af938a180318446276fe7fcf
betalantz <betalantz@gmail.com>
2021-09-03 14:26:43 -0700

    create membership done

--- phase-4/phase-4-rails-practice-challenge-gym/app/controllers/memberships_controller.rb
+++ phase-4/phase-4-rails-practice-challenge-gym/app/controllers/memberships_controller.rb
@@ -1,2 +1,15 @@
 class MembershipsController < ApplicationController
+
+    def create
+        membership = Membership.create!(membership_params)
+        render json: membership, status: :created
+    end
+
+    private
+
+    def membership_params
+        params.permit(:client_id, :gym_id, :charge)
+    end
+
 end
+
